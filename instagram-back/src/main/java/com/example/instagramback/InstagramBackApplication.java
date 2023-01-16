f230fec6ce3468f3e9aba6d253c0443d376f5f6d
vaskovskyAS <lolwwka@gmail.com>
2023-01-16 21:31:24 +0300

    Initial commit

---
+++ instagram-back/src/main/java/com/example/instagramback/InstagramBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.instagramback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstagramBackApplication.class, args);
+    }
+
+}
---
+++ instagram-back/src/test/java/com/example/instagramback/InstagramBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.instagramback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
