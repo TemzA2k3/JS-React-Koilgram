acf61fc4bdbdf0b30ea1d9bcda8e2ffa8f835bf7
vaskovskyAS <lolwwka@gmail.com>
2023-01-26 00:38:11 +0300

    Cors Filter

---
+++ instagram-back/src/main/java/com/example/instagramback/filter/CustomCorsFilter.java
@@ -0,0 +1,53 @@
+package com.example.instagramback.filter;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import com.example.instagramback.properties.UiProperties;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CustomCorsFilter implements Filter {
+
+    private final UiProperties uiProperties;
+
+    public CustomCorsFilter(UiProperties uiProperties) {
+        this.uiProperties = uiProperties;
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        final List<String> allowedOrigins = List.of(uiProperties.getUrl());
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+        String origin = request.getHeader("Origin");
+        response.setHeader("Access-Control-Allow-Origin", allowedOrigins.contains(origin) ? origin : "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods",
+            "ACL, CANCELUPLOAD, CHECKIN, CHECKOUT, COPY, DELETE, GET, HEAD, LOCK, MKCALENDAR, MKCOL, MOVE, OPTIONS, POST, PROPFIND, PROPPATCH, PUT, REPORT, SEARCH, UNCHECKOUT, UNLOCK, UPDATE, VERSION-CONTROL");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept, Key, Authorization, Login");
+
+        if (request.getMethod().equals("OPTIONS")) {
+            response.setHeader("Access-Control-Max-Age", "86400");
+            response.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+        else {
+            chain.doFilter(req, res);
+        }
+    }
+}
+
