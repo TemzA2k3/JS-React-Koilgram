e831f57de3753fadffc08bcb440dd55583ca73c9
TemzA2k3 <97469563+TemzA2k3@users.noreply.github.com>
2023-04-18 00:34:20 +0300

    add main ltl

---
+++ instagram-front/src/components/MainPage/NavbarComponent/Navbar.js
@@ -0,0 +1,20 @@
+
+import './Navbar.css'
+import burg from '../../../images/burg.png'
+
+const Navbar = () => {
+    return (
+        <div className="nav">
+            <img className='img' src={burg}></img>
+            <p>dfhwefwef</p>
+            <p>dfhwefwef</p>
+            <p>dfhwefwef</p>
+            <p>dfhwefwef</p>
+            <p>dfhwefwef</p>
+            <p>dfhwefwef</p>
+
+        </div>
+    )
+}
+
+export default Navbar
--- instagram-front/src/components/app/app.js
+++ instagram-front/src/components/app/app.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import LogInForm from '../Registration-LogIn-Component/LogInForm/LogInForm';
 import RegistrationForm from '../Registration-LogIn-Component/RegistrationForm/RegistrationForm';
+import Navbar from '../MainPage/NavbarComponent/Navbar';
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
                     <Route path='/koilgram/'>
                         <Route path="login" element={<LogInForm />} />
                         <Route path="register" element={<RegistrationForm />} />
+                        <Route path="mainpage" element={<Navbar/>} />
                     </Route>
                 </Routes>
             </div>
