556b16c6a0f250921ea5f64b8efc2e93a1937a46
Artem <kikotik91@gmail.com>
2023-05-13 20:26:44 +0300

    add new design

--- instagram-front/src/components/Registration-LogIn-Component/LogInForm/LogInForm.js
+++ instagram-front/src/components/Registration-LogIn-Component/LogInForm/LogInForm.js
@@ -10,8 +10,8 @@ const LogInForm = () => {
 
     const remEff = () => {
         setTimeout(() => {
-        let wrapper = document.querySelector('.wrapper-log')
-        wrapper.classList.add('active')
+            let wrapper = document.querySelector('.wrapper-log')
+            wrapper.classNameList.add('active')
         }, 0)
     }
 
@@ -19,32 +19,67 @@ const LogInForm = () => {
 
     return (
         <div className="outer">
-            <div className="wrapper wrapper-log">
-                <div className="form-box login">
-                    <h2>Login</h2>
-                    <form action="#">
-                        <div className="input-box">
-                            <span className="icon"><ion-icon name="mail" /></span>
-                            <input type="text" required />
-                            <label>Email</label>
+            <div className="slider">
+                <h1>hfbwf</h1>
+            </div>
+            <div className="form">
+                <div className="wrapper wrapper-reg">
+                    <div className="form-box login">
+                        <div className="name">
+                            <h1>Koil<span>Gram</span></h1>
                         </div>
-                        <div className="input-box">
-                            <span className="icon"><ion-icon name="lock-closed" /></span>
-                            <input type="password" required />
-                            <label>Password</label>
-                        </div>
-                        <div className="remember-forgot">
-                            <label><input type="checkbox" />Remember me</label>
-                            <a href="#">Forgot password?</a>
-                        </div>
-                        <button type="submit" className="btn" style={{ 'background': '#162938', 'color': '#fff' }}>Login</button>
-                        <div className="login-register">
-                            <p>Don't have an account?<Link to="/koilgram/register" className="register-link">Register</Link></p>
-                        </div>
-                    </form>
+                        <h2>Login</h2>
+                        <form action="#">
+                            <div className="input-box">
+                                <span className="icon"><ion-icon name="mail" /></span>
+                                <input type="text" required />
+                                <label>Email</label>
+                            </div>
+                            <div className="input-box">
+                                <span className="icon"><ion-icon name="lock-closed" /></span>
+                                <input type="password" required />
+                                <label>Password</label>
+                            </div>
+                            <div className="remember-forgot">
+                                <label><input type="checkbox" />Remember me</label>
+                                <a href="#">Forgot password?</a>
+                            </div>
+                            <button type="submit" className="btn" style={{ 'background': '#0d60a0', 'color': '#fff' }}>Login</button>
+                            <div className="login-register">
+                                <p>Don't have an account?<Link to="/koilgram/register" classNameName="register-link">Register</Link></p>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
+        // <div classNameName="outer">
+        //     <div classNameName="wrapper wrapper-log">
+        //         <div classNameName="form-box login">
+        //             <h2>Login</h2>
+        //             <form action="#">
+        //                 <div classNameName="input-box">
+        //                     <span classNameName="icon"><ion-icon name="mail" /></span>
+        //                     <input type="text" required />
+        //                     <label>Email</label>
+        //                 </div>
+        //                 <div classNameName="input-box">
+        //                     <span classNameName="icon"><ion-icon name="lock-closed" /></span>
+        //                     <input type="password" required />
+        //                     <label>Password</label>
+        //                 </div>
+        //                 <div classNameName="remember-forgot">
+        //                     <label><input type="checkbox" />Remember me</label>
+        //                     <a href="#">Forgot password?</a>
+        //                 </div>
+        //                 <button type="submit" classNameName="btn" style={{ 'background': '#162938', 'color': '#fff' }}>Login</button>
+        //                 <div classNameName="login-register">
+        //                     <p>Don't have an account?<Link to="/koilgram/register" classNameName="register-link">Register</Link></p>
+        //                 </div>
+        //             </form>
+        //         </div>
+        //     </div>
+        // </div>
     )
 }
 
